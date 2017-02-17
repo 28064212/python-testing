2fc224bb0cf6c049fd75397e154a357de3115b75
28064212 <halligf2@gmail.com>
2017-02-17 23:22:00 +0000

    testing requests as a requirement

--- pythontesting/pythontesting.py
+++ pythontesting/pythontesting.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
-
 import sys
+import requests
 
 def main():
-	print("hello from Python %s" % (sys.version,))
-	
-if __name__ == "__main__":
-	main()
+    print('Hello from Python', sys.version)
+    r = requests.get('https://api.github.com/repos/28064212/python-testing')
+    print('git name:', r.json()['full_name'])
+    print('last update:', r.json()['pushed_at'])
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
 setup(
-	name='python-testing',
-	version='0.0.1',
-	url='https://github.com/28064212/python-testing',
-	# To provide executable scripts, use entry points in preference to the
+    name = 'python-testing',
+    version = '0.0.2',
+    url = 'https://github.com/28064212/python-testing',
+    # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
+    entry_points = {
         'console_scripts': [
             'pythontesting = pythontesting',
         ],
-	},
+    },
+    install_requires = [
+        'requests',
+    ],
 )
