739dd67703bf1da583d1cb38886a59e3e978e6c9
28064212 <halligf2@gmail.com>
2017-02-18 22:51:54 +0000

    rename package to remove hyphen in accordance with PEP8

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name = 'python-testing',
-    version = '0.0.2',
+    name = 'pythontesting',
+    version = '0.0.3',
     url = 'https://github.com/28064212/python-testing',
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
